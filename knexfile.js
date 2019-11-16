1b14277968320db7561153de6871ea48285b870c
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-16 14:56:56 +0000

    Add pg support

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/friendfinder.db3'
+      filename: "./data/friendfinder.db3"
     },
     useNullAsDefault: true,
     migrations: {
@@ -16,9 +15,9 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); 
-      },
-    },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
 
   testing: {
@@ -35,41 +34,35 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); 
-      },
-    },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
-
 };
