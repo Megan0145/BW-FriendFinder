f34e73c92627a12f04f8a51eb2e94a80e89a46ae
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-16 13:39:21 +0000

    Add auth model file, add and findById helper functions

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use("/api/auth", authRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome to Friend Finder API!");
---
+++ authentication/auth-model.js
@@ -0,0 +1,18 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  findById,
+  add
+};
+
+function add(user) {
+  return db("users")
+    .insert(user)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function findById(id) {
+  return db("users").where({ id });
+}
