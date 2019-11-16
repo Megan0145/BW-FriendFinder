62dcda2ad60837de60de333a50ea4ef70e9dec96
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-16 14:42:50 +0000

    Plug token generator function into login endpoint, sends back jwt on successful login

--- authentication/auth-router.js
+++ authentication/auth-router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const users = require("./auth-model");
 const bcrypt = require("bcryptjs");
-const { validateUser } = require("../middleware");
+const { validateUser } = require("../middleware/index");
+const generateToken = require("../middleware/tokenGenerator");
 
 router.post("/register", validateUser, (req, res) => {
   const hashedpw = bcrypt.hashSync(req.body.password, 10);
@@ -29,15 +30,14 @@ router.post("/login", validateUser, (req, res) => {
     .findByUsername(username)
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res
           .status(200)
-          .json({ message: `Welcome back ${user.username}!`, user });
+          .json({ message: `Welcome back ${user.username}!`, user, token });
       } else {
-        res
-          .status(401)
-          .json({
-            message: "Could not find user. Invalid credentials provided"
-          });
+        res.status(401).json({
+          message: "Could not find user. Invalid credentials provided"
+        });
       }
     })
     .catch(err => {
