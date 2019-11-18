8d2df93789562f94f64e875adad5a81b78237e68
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-18 17:16:14 +0000

    Add intermediate

---
+++ data/migrations/20191118170927_add-intermediate-table.js
@@ -0,0 +1,27 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("user_answers", tbl => {
+    tbl.increments();
+    tbl
+      .integer("user_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      ,inTable("users");
+    tbl
+      .integer("question_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      ,inTable("question");
+    tbl
+      .integer("answer_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      ,inTable("answers");
+  });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("user-answers");
+};
