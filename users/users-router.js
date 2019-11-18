f66ec4a59215cf40d36c77087afb9989fc8ac290
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-18 12:32:53 +0000

    Add users router, helper method to get all users in db and endpoint to get all users

---
+++ users/users-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const users = require("./users-model");
+
+router.get("/", (req, res) => {
+  users
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not get users: " + err.message });
+    });
+});
+
+module.exports = router;
