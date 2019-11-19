0f0af849124a7c51f8ba4d3b4619318aaebeab71
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-19 14:13:17 +0000

    New seed for messages

--- users/users-model.js
+++ users/users-model.js
@@ -12,3 +12,16 @@ function findUsers() {
 function findUserById(id) {
   return db("users").where({ id }).first();
 }
+
+// SELECT m.id AS message_id,
+//        m.receiver_id AS receiver_id,
+//        u.id AS sender_id,
+//        u.username AS sender_username,
+//        m.message
+//   FROM messages AS m
+//        JOIN
+//        users AS u ON m.sender_id = u.id
+//  WHERE m.receiver_id = 1;
+function findMessagesByUserId(id) {
+    return db("messages as m")
+}
