8a558887fd9c3a073e66984f339f8cde2d54155f
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-19 14:47:42 +0000

    Find messages by user id

--- users/users-model.js
+++ users/users-model.js
@@ -11,21 +11,19 @@ function findUsers() {
 }
 
 function findUserById(id) {
-  return db("users").where({ id }).first();
+  return db("users")
+    .where({ id })
+    .first();
 }
 
-// SELECT m.id AS message_id,
-//        m.receiver_id AS receiver_id,
-//        u.id AS sender_id,
-//        u.username AS sender_username,
-//        m.message
-//   FROM messages AS m
-//        JOIN
-//        users AS u ON m.sender_id = u.id
-//  WHERE m.receiver_id = 1;
 function findMessagesByUserId(id) {
-    return db("messages as m")
+  return db("messages as m")
     .join("users as u", "m.sender_id", "u.id")
-    .select("m.receiver_id AS receiver_id", " u.id AS sender_id", "u.username AS sender_username", "m.message")
-    .where("m.receiver_id", id)
+    .select(
+      "m.receiver_id AS receiver_id",
+      " u.id AS sender_id",
+      "u.username AS sender_username",
+      "m.message"
+    )
+    .where("m.receiver_id", id);
 }
