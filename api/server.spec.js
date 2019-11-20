d6540e075462eb881cf0e68ee7bb811bc23cb9e9
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-20 21:28:15 +0000

    Test: Register endpoint returns 401 when credentials of already existent user exists

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,11 +1,16 @@
 const request = require("supertest");
 const server = require("./server");
+const db = require("../data/db-config");
+
+beforeEach(() => {
+    return db("users").truncate()
+})
 
 describe("server", () => {
   describe("[GET] / endpoint", () => {
-    // test("the db env is testing", () => {
-    //   expect(process.env.DB_ENV).toBe("testing");
-    // });
+    test("the db env is testing", () => {
+      expect(process.env.DB_ENV).toBe("testing");
+    });
     test("Returns 200 OK", () => {
       return request(server)
         .get("/")
@@ -26,5 +31,27 @@ describe("server", () => {
           .send({ username: "Testing", password: "1234" })
           .expect(201);
       });
+      test("Register endpoint returns 401 when invalid credentials provided: user already exists", () => {
+        return request(server)
+          .post("/api/auth/register")
+          .send([{ username: "Testing", password: "1234" }, { username: "Testing", password: "1234" }])
+          .expect(201);
+      });
   });
+
+  describe("[POST] /login endpoint", () => {
+    test("Login endpoint returns 401 when no credentials provided", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .expect(401)
+        .expect({"message":"Missing user data"});
+    });
+    test("Login endpoint returns 201 when valid credentials provided", async () => {
+        return request(server)
+          .post("/api/auth/register")
+          .send({ username: "Testing", password: "1234" })
+          .expect(201);
+      });
+  });
+
 });
