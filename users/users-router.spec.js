977d06b58ac74a063fb4b86bf9511b82be279ebd
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-20 22:03:20 +0000

    Test: Get all users endpoint returns 401 if no token provided in header

--- users/users-router.spec.js
+++ users/users-router.spec.js
@@ -2,33 +2,19 @@ const request = require("supertest");
 const server = require("../api/server");
 const db = require("../data/db-config");
 
-beforeEach(() => {
-  return db("users").truncate();
+beforeEach(async () => {
+await db("users").truncate()
+await db("users").insert({username: "Megan", password: "1234"});
 });
 
-describe("Auth router", () => {
-  describe("[POST] /register endpoint", () => {
-    test("Register endpoint returns 401 when no credentials provided", () => {
+describe("Users router", () => {
+  describe("[GET] / endpoint", () => {
+    test("Should not return all users if not token provided", () => {
       return request(server)
-        .post("/api/auth/register")
+        .get("/api/users")
         .expect(401)
-        .expect({ message: "Missing user data" });
+        .expect({ message: 'You shall not pass! No credentials provided' });
     });
-    // test("Register endpoint returns 201 when valid credentials provided", () => {
-    //   return request(server)
-    //     .post("/api/auth/register")
-    //     .send({ username: "Testing", password: "1234" })
-    //     .expect(201);
-    // });
-    // test("Register endpoint returns 401 when invalid credentials provided: user already exists", () => {
-    //   return request(server)
-    //     .post("/api/auth/register")
-    //     .send([
-    //       { username: "Testing", password: "1234" },
-    //       { username: "Testing", password: "1234" }
-    //     ])
-    //     .expect(401);
-    // });
   });
 
   describe("[POST] /login endpoint", () => {
