203924fd255bc27aad714aedc5dd0bf43dd7b41e
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-20 12:52:33 +0000

    Add restricted middleware and plug into users router

--- api/server.js
+++ api/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const authRouter = require("../authentication/auth-router");
 const usersRouter = require("../users/users-router");
 
+const authenticate = require("../middleware/restricted")
+
 const server = express();
 
 server.use(helmet());
@@ -12,7 +14,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-server.use("/api/users", usersRouter);
+server.use("/api/users", authenticate, usersRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome to Friend Finder API!");
