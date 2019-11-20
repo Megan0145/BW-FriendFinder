c9a4ff96b52e2e5f8f8872f830c4d3db638d9959
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-20 22:09:21 +0000

    Test: get / returns all users if valid token provided

--- users/users-router.spec.js
+++ users/users-router.spec.js
@@ -3,17 +3,26 @@ const server = require("../api/server");
 const db = require("../data/db-config");
 
 beforeEach(async () => {
-await db("users").truncate()
-await db("users").insert({username: "Megan", password: "1234"});
+  await db("users").truncate();
+  await db("users").insert({ username: "Megan", password: "1234" });
 });
 
 describe("Users router", () => {
   describe("[GET] / endpoint", () => {
-    test("Should not return all users if not token provided", () => {
+    test("Should not return all users if no token provided", () => {
       return request(server)
         .get("/api/users")
         .expect(401)
-        .expect({ message: 'You shall not pass! No credentials provided' });
+        .expect({ message: "You shall not pass! No credentials provided" });
+    });
+    test("Should return all users if valid token provided", () => {
+      return request(server)
+        .get("/api/users")
+        .set(
+          "Authorization",
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxOSwidXNlcm5hbWUiOiJtZWdhbiIsImlhdCI6MTU3NDI2OTUzNCwiZXhwIjoxNTc0MzU1OTM0fQ.mckdVqdZd8KfbKnvIJEK4ngDqwOuOt_2zics4rDLLiw"
+        )
+        .expect(200);
     });
   });
 
