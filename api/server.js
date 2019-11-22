4f63f34f76602df19cbc40ae2007ec1a60df1394
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-22 13:10:04 +0000

    Endpoint to get all users for homepage

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const users = require("../users/users-model");
 
 const authRouter = require("../authentication/auth-router");
 const usersRouter = require("../users/users-router");
@@ -19,5 +20,15 @@ server.use("/api/users", authenticate, usersRouter);
 server.get("/", (req, res) => {
   res.send("Welcome to Friend Finder API!");
 });
+
+server.get("/api/home", (req, res) => {
+  users.findUsers()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
 
 module.exports = server;
